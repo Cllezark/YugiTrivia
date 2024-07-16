2c34649e63eb2163904cb76d1e780cbadd06d1eb
Clifford Lezark <Clifford.Lezark@MyFWC.com>
2024-07-16 15:03:56 -0400

    created TriviaCard class, the basis of all db interactions.

---
+++ Shared/TriviaCard.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YugiTrivia.Shared
+{
+    public class TriviaCard
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public DateOnly? TcgDate { get; set; }
+        public DateOnly? OcgDate { get; set; }
+        public string? Artwork { get; set; }
+    }
+}
