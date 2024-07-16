729cca30c5a86008e7f3ed567bf8b9258c5453e1
Clifford Lezark <Clifford.Lezark@MyFWC.com>
2024-07-16 16:39:46 -0400

    swtiched TriviaCard.TcgDate and TriviaCard.OcgDate to strings.

--- Shared/TriviaCard.cs
+++ Shared/TriviaCard.cs
@@ -8,11 +8,11 @@ namespace YugiTrivia.Shared
 {
     public class TriviaCard
     {
-        public int Id { get; set; }
+        public long Id { get; set; }
         public string Name { get; set; }
         public string Type { get; set; }
-        public DateOnly? TcgDate { get; set; }
-        public DateOnly? OcgDate { get; set; }
+        public string TcgDate { get; set; }
+        public string OcgDate { get; set; }
         public string? Artwork { get; set; }
     }
 }
