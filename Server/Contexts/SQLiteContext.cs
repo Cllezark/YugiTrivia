2ab8031c4df77ee6741247c50aa6b0997e6d7559
Clifford Lezark <Clifford.Lezark@MyFWC.com>
2024-07-16 14:39:14 -0400

    created sqlite context

---
+++ Server/Contexts/SQLiteContext.cs
@@ -0,0 +1,17 @@
+﻿namespace YugiTrivia.Server.Contexts;
+using Microsoft.EntityFrameworkCore;
+public class SQLiteContext : DbContext
+{
+    protected readonly IConfiguration _configuration;
+
+    public SQLiteContext(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        options.UseSqlite(_configuration.GetConnectionString("CardsDB"));
+    }
+
+}
